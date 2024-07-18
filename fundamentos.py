502b4b1ff7a7fa304bab900742aa0b4106a85576
EGO <ego.argote@hotmail.com>
2024-07-17 23:34:10 -0500

    se abordaron fundamentos de Python

---
+++ fundamentos.py
@@ -0,0 +1,13 @@
+name = "Diego"
+print(type(name))
+print(name[0])
+print(name[1])
+print(name[2])
+
+last_name = "Argote Ortiz"
+print(name * 5)
+print(len(name))
+print(name.lower())
+print(name.upper())
+print(name.strip())
+print(name+" "+last_name)
