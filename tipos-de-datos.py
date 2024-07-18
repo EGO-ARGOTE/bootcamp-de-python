d5d7b00794d611fa92da5465a124dfa3583e6a23
EGO <ego.argote@hotmail.com>
2024-07-17 21:36:31 -0500

    Inicializa Python

---
+++ tipos-de-datos.py
@@ -0,0 +1,18 @@
+"string" #se utiliza para tipos de datos de texto
+'string'
+'''string'''
+"""string"""
+"""
+    Tus datos son
+    nombre: Diego
+    apellido: Argote
+"""
+
+#datos numericos
+4 #entero
+4.5 #flotante
+
+#booleanos
+True # T mayuscula
+False # F mayuscula
+
