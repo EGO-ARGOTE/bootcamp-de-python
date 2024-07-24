f1630e6588038501ba605908919b7fa81a5911db
EGO <ego.argote@hotmail.com>
2024-07-23 19:42:52 -0500

    abordamos operadores aritmeticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,28 @@
+# Sumas y restas (+ y -)
+suma = 2024 + 1996
+print(suma)
+
+resta = 2024 - 1996
+print(resta)
+
+# Multiplicacion y division (* y /)
+multiplicacion = 23 * 7
+print(multiplicacion)
+
+division_entera = 8 / 2
+print(division_entera)
+
+# Potencia (**)
+potencia = 2 ** 3
+print(potencia)
+
+# Division entera (//)
+division = 12 // 4
+print(division)
+
+# Modulo (%)
+modulo = 12 % 4
+print(modulo)
+
+tipo_de_dato = type(division_entera)
+print(tipo_de_dato)
