1a3ebd5d0d883b7e489903708d6ba5e4ccd828dc
EGO <ego.argote@hotmail.com>
2024-07-24 19:16:18 -0500

    abordamos operadores de comparacion

---
+++ operadores_de_comparacion.py
@@ -0,0 +1,44 @@
+es_igual_que = 5 == 5
+print(es_igual_que)
+
+es_diferente_que = 5 != 6
+print(es_diferente_que)
+
+es_mayor_que = 5 > 6
+print(es_mayor_que)
+
+es_mayor_igual_que = 5 >= 6
+print(es_mayor_igual_que)
+
+es_menor_que = 5 < 6
+print(es_menor_que)
+
+es_menor_igual_que = 5 <= 6
+print(es_menor_igual_que)
+
+#Calculos combinados
+a = 5
+b = 10
+c = 20
+comparacion = a + b == c
+print(comparacion)
+
+mio = 1996
+novia = 1999
+ex = 1996
+
+comparacion_2 = mio + novia == ex
+print(comparacion_2)
+
+#Comparar usuarios
+contrasena_almacenada = "123456"
+contrasena_escrita = "123456"
+print(contrasena_almacenada == contrasena_escrita) #True
+
+contrasena_almacenada_2 = "123456"
+contrasena_escrita_2 = '123456'
+print(contrasena_almacenada_2 == contrasena_escrita_2) #True
+
+contrasena_almacenada_3 = "123456"
+contrasena_escrita_3 = '''123456'''
+print(contrasena_almacenada_3 == contrasena_escrita_3) #True
