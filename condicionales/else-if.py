3f034d8b3c827f256c3e5986d1ffdcaf333dff77
EGO <ego.argote@hotmail.com>
2024-07-31 02:43:02 -0500

    abordamos condicionales

---
+++ condicionales/else-if.py
@@ -0,0 +1,23 @@
+ingreso_mensual = 10000
+ahorro_mensual = 7000
+
+if ingreso_mensual > 1000:
+    if ahorro_mensual < 5000:
+        print ("Estas bien")
+    else:
+        print ("Gastas mucho")
+    
+elif ingreso_mensual > 1000:
+    print ("Estas bien en el latinoaemrica")
+    
+elif ingreso_mensual > 1000000:
+    print ("Estas bien en el mundo")
+    
+elif ingreso_mensual > 10000000:
+    print ("Mejor que Elon Musk")
+    
+elif ingreso_mensual > 100000000:
+    print ("Eres rico")
+    
+else:
+    print ("Eres pobre")
---
+++ condicionales/if-else.py
@@ -0,0 +1,7 @@
+contrasena_almacenada = "ego123"
+contrasena_escrita = "ego123"
+
+if contrasena_almacenada == contrasena_escrita:
+    print("Acceso permitido")
+else:
+    print("Acceso denegado")
