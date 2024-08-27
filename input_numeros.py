9ffee787274640f20c9d2883f203cfca7be17439
EGO <ego.argote@hotmail.com>
2024-08-27 10:03:14 -0500

    abordamos inputs

---
+++ input_numeros.py
@@ -0,0 +1,11 @@
+#Pedirle un numero al usuario
+numero = input("¿Cual es tu numero campista?: ")
+
+# Multiplicarlo por 2
+# resultado = int(numero) * 2
+
+#Combirtiendo el numero entero a float, tiene en cuenta el punto
+resultado = float(numero) * 2
+
+#Mostrar el resultado
+print(resultado)
